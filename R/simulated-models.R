dd1cbdee7a810f98e0e51b51f787c19cf405eb04
DavidSolan0 <jdsolanov@unal.edu.co>
2025-10-13 21:28:07 +0200

    format and bug sampling outliers in simulated-models

--- R/simulated-models.R
+++ R/simulated-models.R
@@ -1,4 +1,3 @@
-
 #* uncontaminated model
 
 uncontaminated <- function(rho, m = 30, n = 200, t = c(-0.5, 1.5), plot = TRUE) {
@@ -13,14 +12,21 @@ uncontaminated <- function(rho, m = 30, n = 200, t = c(-0.5, 1.5), plot = TRUE) 
   #   fdata object for uncontaminated model
 
   l <- length((-n + 1):n)
-  b <- arima.sim(list(order = c(1, 0, 0), ar = rho), n = l, innov = rnorm(l, 0, 1))
+  b <- arima.sim(
+    list(order = c(1, 0, 0), ar = rho),
+    n = l,
+    innov = rnorm(l, 0, 1)
+  )
   TT <- seq(from = t[1], to = t[2], length = m) #* tiempos en los que se observa
 
   xx <- matrix(nrow = m, ncol = l)
   xx[, 1] <- cos(pi * TT)
   for (i in 2:l)
   {
-    xx[, i] <- cos(pi * TT) * (1 - rho) + rho * xx[, i - 1] + rnorm(1, 0, 0.3) * sin(pi * TT) + b[i]
+    xx[, i] <- cos(pi * TT) * (1 - rho) +
+      rho * xx[, i - 1] +
+      rnorm(1, 0, 0.3) * sin(pi * TT) +
+      b[i]
   }
 
   x <- xx[, (l - (n - 1)):l]
@@ -31,20 +37,25 @@ uncontaminated <- function(rho, m = 30, n = 200, t = c(-0.5, 1.5), plot = TRUE) 
 
 
   if (plot == TRUE) {
-    x11()
     plot(fdataobj,
       col = "grey", ylab = "Xi(t)", xlab = "t",
       ylim = c(-30, 35), main = "Uncontaminated model"
     )
     last <- n
-    x11()
+
     plot.ts(c(x[, (n - last + 1):n]),
-      ylim = c(min(x[, (n - last):n]) - 0.5, 0.5
-      + max(x[, (n - last):n])), axes = F, xlab = "", ylab = "", lwd = 1,
+      ylim = c(
+        min(x[, (n - last):n]) - 0.5,
+        max(x[, (n - last):n]) + 0.5
+      ),
+      axes = FALSE,
+      xlab = "",
+      ylab = "",
+      lwd = 1,
       main = "no contaminated time serie"
     )
     axis(2)
-    axis(1, tick = F, labels = F)
+    axis(1, tick = FALSE, labels = FALSE)
     abline(h = 0)
     box()
   }
@@ -71,8 +82,7 @@ magnitude <- function(rho, k = 10, plot = TRUE, t = c(-0.5, 1.5)) {
 
   m <- ncol(fdataobj)
   n <- nrow(fdataobj)
-  o <- runifdisc(n = 3, max = n, min = 1)
-
+  o <- sample(1:n, 3)
 
   Xi3 <- x <- fdataobj[["data"]]
 
@@ -82,7 +92,6 @@ magnitude <- function(rho, k = 10, plot = TRUE, t = c(-0.5, 1.5)) {
   fdataobj <- fdata(Xi3, argvals = TT, rangeval = c(min(TT), max(TT)))
 
   if (plot == TRUE) {
-    x11()
     plot(fdataobj,
       col = "grey", ylab = "Xi(t)", xlab = "t",
       ylim = c(-30, 35), main = "magnitude outliers"
@@ -90,14 +99,20 @@ magnitude <- function(rho, k = 10, plot = TRUE, t = c(-0.5, 1.5)) {
     lines(fdataobj[o, ], col = "black", lty = 1, lwd = 2)
 
     last <- n
-    x11()
+
     plot.ts(c(t(Xi3[(n - last + 1):n, ])),
-      ylim = c(min(t(Xi3[(n - last):n, ])) - 0.5, 0.5
-      + max(t(Xi3[(n - last):n, ]))), axes = F, xlab = "", ylab = "", lwd = 1,
+      ylim = c(
+        min(t(Xi3[(n - last):n, ])) - 0.5,
+        max(t(Xi3[(n - last):n, ])) + 0.5
+      ),
+      axes = FALSE,
+      xlab = "",
+      ylab = "",
+      lwd = 1,
       main = "time serie with magnitude ouliers"
     )
     axis(2)
-    axis(1, tick = F, labels = F)
+    axis(1, tick = FALSE, labels = FALSE)
     abline(h = 0)
     abline(v = o * 30, lty = 2)
     box()
@@ -124,31 +139,36 @@ shape <- function(rho, k = 4, plot = TRUE, t = c(-0.5, 1.5)) {
 
   m <- ncol(fdataobj)
   n <- nrow(fdataobj)
-  I1 <- sample(1:n, 1)
-  I2 <- sample(1:n, 1)
-  I3 <- sample(1:n, 1)
-  o <- runifdisc(n = 3, max = n, min = 1)
+  o <- sample(1:n, 3)
 
   Xi4 <- x <- fdataobj[["data"]]
 
   TT <- seq(from = t[1], to = t[2], length = m)
 
-  for (i in 1:length(o)) {
+  for (i in seq_along(o)) {
     Xi4[o[i], ] <- x[o[i], ] + k * cos(3 * pi * TT)
   }
 
   fdataobj <- fdata(Xi4, argvals = TT, rangeval = c(min(TT), max(TT)))
 
   if (plot == TRUE) {
-    x11()
     plot(fdataobj, col = "grey", ylab = "Xi(t)", xlab = "t", ylim = c(-30, 35))
     lines(fdataobj[o, ], col = "black", lty = 1, lwd = 2)
-    x11()
+
     last <- n
-    plot.ts(c(t(Xi4[(n - last + 1):n, ])), ylim = c(min(t(Xi4[(n - last):n, ])) - 0.5, 0.5
-    + max(t(Xi4[(n - last):n, ]))), axes = F, xlab = "", ylab = "", lwd = 1)
+    plot.ts(
+      c(t(Xi4[(n - last + 1):n, ])),
+      ylim = c(
+        min(t(Xi4[(n - last):n, ])) - 0.5,
+        0.5 + max(t(Xi4[(n - last):n, ]))
+      ),
+      axes = FALSE,
+      xlab = "",
+      ylab = "",
+      lwd = 1
+    )
     axis(2)
-    axis(1, tick = F, labels = F)
+    axis(1, tick = FALSE, labels = FALSE)
     abline(h = 0)
     abline(v = o * 30, lty = 2)
     box()
@@ -175,17 +195,14 @@ partial <- function(rho, k = 10, plot = TRUE, t = c(-0.5, 1.5)) {
 
   m <- ncol(fdataobj)
   n <- nrow(fdataobj)
-  I1 <- sample(1:n, 1)
-  I2 <- sample(1:n, 1)
-  I3 <- sample(1:n, 1)
-  o <- runifdisc(n = 3, max = n, min = 1)
+  o <- sample(1:n, 3)
 
   Xi5 <- x <- fdataobj[["data"]]
 
 
   TT <- seq(from = t[1], to = t[2], length = m)
 
-  for (i in 1:length(o)) {
+  for (i in seq_along(o)) {
     Ti <- runif(1, -0.5, 1.5)
     change <- TT >= Ti
     Xi5[o[i], change] <- x[o[i], change] + k
@@ -194,15 +211,23 @@ partial <- function(rho, k = 10, plot = TRUE, t = c(-0.5, 1.5)) {
   fdataobj <- fdata(Xi5, argvals = TT, rangeval = c(min(TT), max(TT)))
 
   if (plot == TRUE) {
-    x11()
     plot(fdataobj, col = "grey", ylab = "Xi(t)", xlab = "t", ylim = c(-30, 35))
     lines(fdataobj[o, ], col = "black", lty = 1, lwd = 2)
-    x11()
+
     last <- n
-    plot.ts(c(t(Xi5[(n - last + 1):n, ])), ylim = c(min(t(Xi5[(n - last):n, ])) - 0.5, 0.5
-    + max(t(Xi5[(n - last):n, ]))), axes = F, xlab = "", ylab = "", lwd = 1)
+    plot.ts(
+      c(t(Xi5[(n - last + 1):n, ])),
+      ylim = c(
+        min(t(Xi5[(n - last):n, ])) - 0.5,
+        0.5 + max(t(Xi5[(n - last):n, ]))
+      ),
+      axes = FALSE,
+      xlab = "",
+      ylab = "",
+      lwd = 1
+    )
     axis(2)
-    axis(1, tick = F, labels = F)
+    axis(1, tick = FALSE, labels = FALSE)
     abline(h = 0)
     abline(v = o * 30, lty = 2)
     box()
@@ -250,7 +275,6 @@ multifdata <- function(
     )
 
     if (plot == TRUE) {
-      x11()
       par(mfrow = c(1, 3))
       plot(fdataobj, main = "Original process", col = "gray")
       lines(fdataobj[outliers, ], col = "black", lty = 1, lwd = 2)
@@ -263,7 +287,6 @@ multifdata <- function(
     mfdata <- list(original = fdataobj$data, firt = deriv1$data)
 
     if (plot == TRUE) {
-      x11()
       par(mfrow = c(1, 2))
       plot(fdataobj, main = "Original process")
       lines(fdataobj[outliers, ], col = "black", lty = 1, lwd = 2)
@@ -274,7 +297,6 @@ multifdata <- function(
     mfdata <- list(original = fdataobj$data)
 
     if (plot == TRUE) {
-      x11()
       plot(fdataobj, main = "Original process")
       lines(fdataobj[outliers, ], col = "black", lty = 1, lwd = 2)
     }
