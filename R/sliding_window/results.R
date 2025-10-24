0ce473bd7598c76a52940d23112f0cd5c484c5ba
DavidSolan0 <jdsolanov@unal.edu.co>
2025-10-24 13:55:06 +0200

    Add univariate and multivariate models to sliding window results script, including parameters for magnitude, shape, and partial models.

--- R/sliding_window/results.R
+++ R/sliding_window/results.R
@@ -17,6 +17,7 @@ source("R/calculate_rates_results.R")
 source("R/sliding_window/outlier-detection-procedures.R")
 source("R/sliding_window/power-study-functions.R")
 
+#** Univariate model **#
 
 #* Parameters
 M <- 100
@@ -64,5 +65,60 @@ tabla <- run_simulation_sliding_window(
   dfunc = dfunc,
   multivariate = FALSE,
   seed = seed
+)
+tabla
+
+
+#** Multivariate model **#
+
+#* Parameters
+M <- 100
+seed <- 1234
+window_size <- 8
+dfunc <- multiMBD
+threshold <- 0.5
+
+#* Magnitude
+K <- c(10, 15, 20, 25)
+tabla <- run_simulation_sliding_window(
+  K,
+  rho = 0.8,
+  model = magnitude,
+  window_size = window_size,
+  M = M,
+  dfunc = dfunc,
+  multivariate = TRUE,
+  seed = seed,
+  threshold = threshold,
+)
+tabla
+
+#* Shape
+K <- c(4, 5, 6, 7)
+tabla <- run_simulation_sliding_window(
+  K,
+  rho = 0.8,
+  model = shape,
+  window_size = window_size,
+  M = M,
+  dfunc = dfunc,
+  multivariate = TRUE,
+  seed = seed,
+  threshold = threshold,
+)
+tabla
+
+#* Partial
+K <- c(10, 15, 20, 25)
+tabla <- run_simulation_sliding_window(
+  K,
+  rho = 0.8,
+  model = partial,
+  window_size = window_size,
+  M = M,
+  dfunc = dfunc,
+  multivariate = TRUE,
+  seed = seed,
+  threshold = threshold,
 )
 tabla
